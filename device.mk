05f7f106db2e23aa1008e13bc845261691f3c423
Anton Hansson <hansson@google.com>
2018-11-07 16:10:05 +0000

    sakura: Enforce RRO for all resource overlays
    Bug: 118823052
    Test: make
    Change-Id: I579d09480c7a482543ea616dd5c493d825bb98b1
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>
    Signed-Off-by: ZabukaZuzu

--- device.mk
+++ device.mk
@@ -28,8 +28,7 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
+PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Board
 PRODUCT_USES_QCOM_HARDWARE := true
