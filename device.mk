c33591550abd55c2c8595f77cf3a8d12509ea63d
dianlujitao <dianlujitao@gmail.com>
2020-09-21 06:01:05 +0000

    sakura: Provide shim for libdpmframework.so
    * libdpmframework depends on strndup16to8 and strdup8to16 methods.
    
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>
    Signed-off-by: ZabukaZuzu

--- device.mk
+++ device.mk
@@ -383,6 +383,15 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
 
+
+# Shims
+PRODUCT_PACKAGES += \
+    libcutils_shim
+
+# Signapk
+PRODUCT_HOST_PACKAGES += \
+    signapk
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     device/xiaomi/sakura
