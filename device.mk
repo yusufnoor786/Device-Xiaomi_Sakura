6a32e3fdbe06375f9acc4d3c5e510bb3d4d25721
“yusufnoor786” <yusufnoor786@gmail.com>
2020-11-08 12:34:19 +0000

    Sakura: Build Vndk package

--- device.mk
+++ device.mk
@@ -401,8 +401,9 @@ PRODUCT_PACKAGES += \
 
 # VNDK
 PRODUCT_PACKAGES += \
-    vndk_package \
-    com.android.vndk.current.on_vendor
+    vndk-package
+    libgui_vendor \
+    libstdc++.vendor
 
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v27/arm/arch-arm-armv7-a-neon/shared/vndk-core/android.frameworks.sensorservice@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib/android.frameworks.sensorservice@1.0.so \
