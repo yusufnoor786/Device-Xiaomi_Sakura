944eb1c430a0f3a4c8cf86f4937e49bab28b806d
“yusufnoor786” <yusufnoor786@gmail.com>
2020-11-16 09:13:25 +0000

    Sakura: Added 1.3 widevine DRM

--- legion_sakura.mk
+++ legion_sakura.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/sakura/device.mk)
 
 #Gapps
-WITH_GAPPS := true
+#WITH_GAPPS := true
 
 # Inherit from MiuiCamera
 $(call inherit-product, vendor/MiuiCamera/config.mk)
